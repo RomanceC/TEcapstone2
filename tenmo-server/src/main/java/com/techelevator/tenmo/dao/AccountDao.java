e87f3416de89612be3171dc2f442d7a1ca75c7d7
Romance Cox <heyycurlie@gmail.com>
2023-03-15 11:21:53 -0400

    Tenmo capstone 2

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,4 +1,15 @@
 package com.techelevator.tenmo.dao;
 
-public class AccountDao {
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.tenmo.model.Transfer;
+import java.math.BigDecimal;
+import java.security.Principal;
+
+public interface AccountDao {
+
+    Account viewBalance(int userId);
+    Account addToBalance(BigDecimal amtToAdd, int id);
+    Account subtractFromBalance(BigDecimal amtToSubtract, int id);
+    Account findUserById(int userId);
+    Account findAccountById(int id);
 }
